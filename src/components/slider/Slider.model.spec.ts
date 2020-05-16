e68c0713c9eb398451566e9ad298aa2332c36014
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-05-16 17:16:14 +0300

    add slider.model.spec (100% coverage)

--- src/components/slider/Slider.model.spec.ts
+++ src/components/slider/Slider.model.spec.ts
@@ -3,29 +3,77 @@ import SliderModel from "./Slider.model";
 describe("Testing SliderModel.ts", () => {
   const sliderModel = new SliderModel();
 
-  it("is definable", () => {
-    expect(sliderModel).toBeDefined();
-  });
-
   describe("Test setting and getting position value to state", () => {
-    it("is setting position to state", () => {
+    beforeEach(() => {
       sliderModel.setPosition(200);
+    });
 
+    it("is setting position to state", () => {
       const state = sliderModel.getState();
 
       expect(state.position).toBe(200);
     });
-    it("is getting position from state", () => {
-      sliderModel.setPosition(200);
 
+    it("is getting position from state", () => {
       const position = sliderModel.getPosition();
 
       expect(position).toBe(200);
     });
   });
-  //   it("is setting click position to state", () => {
-  //     const button = document.createElement("button");
 
-  //     sliderModel.setClickPosition();
-  //   });
+  describe("Verify position after setting less then min and more then max value", () => {
+    beforeAll(() => {
+      sliderModel.setState({ min: 0, max: 100 });
+    });
+
+    it("Set position more then max", () => {
+      sliderModel.setPosition(200);
+
+      const state = sliderModel.getState();
+
+      expect(state.position).toBe(100);
+    });
+
+    it("Set position less then min", () => {
+      sliderModel.setPosition(-100);
+
+      const state = sliderModel.getState();
+
+      expect(state.position).toBe(0);
+    });
+  });
+
+  describe("Test events actions", () => {
+    beforeEach(() => {
+      sliderModel.setState({ position: 0, clickPosition: 0 });
+    });
+
+    it("is set clickPosition to state on click event", async () => {
+      const clickEvent = new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+        clientX: 200,
+      });
+
+      sliderModel.setClickPosition(clickEvent);
+
+      const state = sliderModel.getState();
+
+      expect(state.clickPosition).toBe(200);
+    });
+
+    it("getDragPosition(): return position on mousemove event ", () => {
+      const moveEvent = new MouseEvent("move", {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+        clientX: 200,
+      });
+
+      const position = sliderModel.getDragPosition(moveEvent);
+
+      expect(position).toBe(200);
+    });
+  });
 });
