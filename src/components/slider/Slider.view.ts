e09319360ec5780ca70c500afb2bdd92e8315ca1
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-05-18 14:52:09 +0300

    Refactoring

--- src/components/slider/Slider.view.ts
+++ src/components/slider/Slider.view.ts
@@ -1,12 +1,13 @@
 export default class SliderView {
   public node: HTMLElement;
   public button: HTMLElement;
+  private onMouseMoveList: Array<Function> = [];
 
   constructor() {
     require("./Slider.sass");
   }
 
-  public createNode(state?: object): void {
+  public createNode(state?: object): HTMLElement {
     this.node = document.createElement("section");
     this.node.classList.add("slider");
 
@@ -14,8 +15,30 @@ export default class SliderView {
     this.button.classList.add("slider-button");
 
     this.node.appendChild(this.button);
+
+    this.addHandlers();
+
+    return this.node;
   }
-  public changeButtonPosition(position: number): void {
+  public onMouseMove(callback: Function): void {
+    this.onMouseMoveList.push(callback);
+  }
+
+  private addHandlers(): void {
+    this.button.addEventListener("mousedown", (event: MouseEvent) => {
+      document.addEventListener("mousemove", this.mouseMoveEvent);
+    });
+
+    document.addEventListener("mouseup", () => {
+      document.removeEventListener("mousemove", this.mouseMoveEvent);
+    });
+  }
+  private mouseMoveEvent(event: MouseEvent): void {
+    this.changeButtonPosition(event.pageX);
+
+    this.onMouseMoveList.forEach((callback: Function) => callback(event.pageX));
+  }
+  private changeButtonPosition(position: number): void {
     this.button.style.left = position + "px";
   }
 }
