90b83e96bfe34167f7c96a97541729e29e3abad0
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-05-19 11:01:38 +0300

    append pug and sass to page

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,8 +1,25 @@
 import Slider from "../../components/slider/Slider";
 
-const slider: Slider = new Slider();
+require("./index.sass");
 
-document.body.appendChild(slider.render());
+const pugTemplate = require("./index.pug");
+const html = pugTemplate();
+const mainContainer = document.createElement("main");
+mainContainer.classList.add("index");
 
-slider.slideTo(20);
-slider.slideTo(40);
+mainContainer.innerHTML = html;
+
+document.body.appendChild(mainContainer);
+
+const slider1: Slider = new Slider();
+const slider2: Slider = new Slider();
+const slider3: Slider = new Slider();
+
+const slider1Contaner = document.querySelector(".slider-1");
+const slider2Contaner = document.querySelector(".slider-2");
+const slider3Contaner = document.querySelector(".slider-3");
+
+slider1Contaner.appendChild(slider1.render());
+slider2Contaner.appendChild(slider2.render());
+slider3Contaner.appendChild(slider3.render());
+slider3Contaner.appendChild(slider3.render());
