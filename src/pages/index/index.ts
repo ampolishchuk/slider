8fad89bdaf39b4c081fd97ecc791f21f49fef4e4
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-06-04 08:33:22 +0300

    Cleared files and renamed folders

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,5 +1,3 @@
-import Slider from "../../components/slider/Slider";
-
 require("./index.sass");
 
 const pugTemplate = require("./index.pug");
@@ -11,16 +9,6 @@ mainContainer.innerHTML = html;
 
 document.body.appendChild(mainContainer);
 
-const slider1 = new Slider({ position: 100 });
-const slider2 = new Slider();
-const slider3 = new Slider();
-
 const contaner1 = document.querySelector(".index-container_1");
 const contaner2 = document.querySelector(".index-container_2");
 const contaner3 = document.querySelector(".index-container_3");
-
-contaner1.appendChild(slider1.render());
-contaner2.appendChild(slider2.render());
-contaner3.appendChild(slider3.render());
-
-slider3.slideTo(199);
