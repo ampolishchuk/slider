c4718f6dd10c4f4c96deb83d6f0ba90db1031599
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-06-06 11:42:13 +0300

    Slider calls added

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,7 +1,8 @@
+import Slider from "../../components/slider/Slider";
+
 require("./index.sass");
 
-const pugTemplate = require("./index.pug");
-const html = pugTemplate();
+const html = require("./index.pug")();
 const mainContainer = document.createElement("main");
 mainContainer.classList.add("index");
 
@@ -12,3 +13,17 @@ document.body.appendChild(mainContainer);
 const contaner1 = document.querySelector(".index-container_1");
 const contaner2 = document.querySelector(".index-container_2");
 const contaner3 = document.querySelector(".index-container_3");
+
+const slider1 = new Slider();
+const slider2 = new Slider({
+  style: "extended",
+  value: 100,
+});
+const slider3 = new Slider({
+  style: "extended",
+  orientation: "horizontal",
+});
+
+contaner1.appendChild(slider1.render());
+contaner2.appendChild(slider2.render());
+contaner3.appendChild(slider3.render());
