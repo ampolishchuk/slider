4b35cf234ebb69de5f1a32c57acacbe0a49cc097
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-06-07 12:00:24 +0300

    Appended method for setting value to model

--- src/components/slider/presenters/Slider.presenter.ts
+++ src/components/slider/presenters/Slider.presenter.ts
@@ -1,3 +1,7 @@
 import Presenter from "../services/Presenter";
 
-export default class SliderPresenter extends Presenter {}
+export default class SliderPresenter extends Presenter {
+  setValue(value: number | string): void {
+    this.$.model.setValue(value);
+  }
+}
---
+++ tests/components/slider/presenters/Slider.presenter.spec.ts
@@ -0,0 +1,17 @@
+import SliderPresenter from "../../../../src/components/slider/presenters/Slider.presenter";
+
+describe("Testing SliderPresenter:", () => {
+  const observer = jasmine.createSpyObj("observer", ["subscribe", "notify"]);
+  const model = jasmine.createSpyObj("model", {
+    setValue: "",
+  });
+  const view = jasmine.createSpyObj("view", ["render"]);
+
+  const presenter = new SliderPresenter({ observer, model, view });
+
+  it("Sets value to model;", () => {
+    presenter.setValue(100);
+
+    expect(model.setValue).toHaveBeenCalledTimes(1);
+  });
+});
